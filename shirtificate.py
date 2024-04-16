91e252e5f104b49282f01c0d5322e6a61f790b6e
ajaychaudhary1124 <ajaychaudhary1124@gmail.com>
2024-04-16 14:54:17 +0530

    C2-updated shirtificate

--- shirtificate.py
+++ shirtificate.py
@@ -7,11 +7,11 @@ class Pdf(FPDF):
         self._pdf.add_page()
         self._pdf.set_font("helvetica", "B", 40)
 
-        self._pdf.cell(0,50, "CS50 Shirtificate", new_x="LMARGIN", new_y="NEXT", align='C')
+        self._pdf.cell(0,50, "Wiley Edge Shirtificate", new_x="LMARGIN", new_y="NEXT", align='C')
         self._pdf.image("shirtificate.png",  w=self._pdf.epw)
         self._pdf.set_font("helvetica","", 25)
         self._pdf.set_text_color(255,255,255)
-        self._pdf.cell(0,-240,f"{name} took CS50" , border=1, align="C")
+        self._pdf.cell(0,-240,f"{name} took Wiley" , border=1, align="C")
 
     def output(self, fname):
         self._pdf.output(fname)
